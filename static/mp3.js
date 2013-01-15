5788073287f31dd81e18671f6f19863db826bab1
Joost Molenaar <j.j.molenaar@gmail.com>
2013-01-15 23:47:43 +0100

    Some more views, time to sleep now

--- static/mp3.js
+++ static/mp3.js
@@ -1,3 +1,8 @@
+
+/*
+ * Model base types 
+ */
+
 var Resource = Backbone.Model.extend({
     url: function() {
         return this.get("url");
@@ -11,12 +16,17 @@ var Resource = Backbone.Model.extend({
 });
 
 var CollectionResource = Resource.extend({
+    //initialize: function() {
+    //    this.get("items").on("reset", this.change.bind(this));
+    //},
+
     defaults: function() {
         return { items: new this.itemType() };
     },
 
     parse: function(obj) {
-        obj.items = this.get("items").reset(obj.items);
+        //obj.items = this.get("items").reset(obj.items);
+        obj.items = new this.itemType(obj.items);
         return obj;
     },
 
@@ -26,6 +36,10 @@ var CollectionResource = Resource.extend({
         return obj;
     }
 });
+
+/*
+ * Model types
+ */
 
 var Track = Resource.extend();
 
@@ -53,12 +67,18 @@ var Users = CollectionResource.extend({
     }
 });
 
-var UserListView = Backbone.View.extend({
-    el: "#users",
+/*
+ * Base view types
+ */
 
-    template: $("#users-template").html(),
+var ListView = Backbone.View.extend({
+    events: {
+        "click a": "itemClicked"
+    },
 
     initialize: function() {
+        if (!this.model)
+            return;
         this.listenTo(this.model, "change", this.render);
     },
 
@@ -67,13 +87,99 @@ var UserListView = Backbone.View.extend({
         var html = Mustache.render(this.template, obj);
         this.$el.html(html);
         return this;
+    },
+
+    itemClicked: function(e) {
+        e.preventDefault();
+        var url = $(e.target).attr("href");
+        var model = this.model.get("items").find(function(item) { return item.get("url") == url; });
+        this.trigger("select", model);
     }
 });
+
+/*
+ * View types
+ */
+var TrackList = ListView.extend({
+    el: "#tracks",
+    template: $("#tracks-template").html()
+});
+
+var AlbumList = ListView.extend({
+    el: "#albums",
+    template: $("#albums-template").html()
+});
+
+var ArtistList = ListView.extend({
+    el: "#artists",
+    template: $("#artists-template").html()
+});
+
+var CollectionList = ListView.extend({
+    el: "#collections",
+    template: $("#collections-template").html()
+});
+
+var UserList = ListView.extend({
+    el: "#users",
+    template: $("#users-template").html()
+});
+
+/*
+ * Application
+ */
 
 USERS = new Users();
 USERS.fetch();
 
-USERSVIEW= new UserListView({
+TRACKLIST = new TrackList();
+TRACKLIST.on("select", function(model) {
+    console && console.log && console.log("track selected");
+    model.dump();
+});
+
+ALBUMLIST = new AlbumList();
+ALBUMLIST.on("select", function(model) {
+    console && console.log && console.log("album elected:");
+    model.dump();
+
+    TRACKLIST.model = model;
+    TRACKLIST.initialize();
+
+    model.fetch();
+});
+
+ARTISTLIST = new ArtistList();
+ARTISTLIST.on("select", function(model) {
+    console && console.log && console.log("artist elected:");
+    model.dump();
+
+    ALBUMLIST.model = model;
+    ALBUMLIST.initialize();
+
+    model.fetch();
+});
+
+COLLECTIONLIST = new CollectionList();
+COLLECTIONLIST.on("select", function(model) {
+    console && console.log && console.log("collection selected:");
+    model.dump();
+
+    ARTISTLIST.model = model;
+    ARTISTLIST.initialize();
+
+    model.fetch();
+});
+
+USERLIST = new UserList({
     model: USERS
 });
- 
+USERLIST.on("select", function(model) {
+    console && console.log && console.log("user selected:");
+    model.dump();
+
+    COLLECTIONLIST.model = model;
+    COLLECTIONLIST.initialize();
+
+    model.fetch();
+}); 
