c74cdf43206f37254920029adc0363e081988799
Joost Molenaar <j.j.molenaar@gmail.com>
2014-01-19 22:49:40 +0100

    Edit setup.py to make use of get_git_version

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ static_dirs = ['conf', 'run', 'static']
 
 import os
 from setuptools import setup
+from setuptools_metadata import get_git_version
 
 setup(
     author='Joost Molenaar',
     author_email='j.j.molenaar@gmail.com',
     url='https://github.com/j0057/mp3',
     name='mp3',
-    version='0.2.0',
+    version=get_git_version('mp3.egg-info/version.txt'),
     packages=['mp3'],
     data_files=[ (root, map(lambda f: root + '/' + f, files))
                  for src_dir in static_dirs
